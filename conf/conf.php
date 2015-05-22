09bff619463cb7893c366bd0797abe3984cd99f4
danielschmitz <danieljfa@gmail.com>
2015-05-22 18:49:05 -0300

    Ajustando o acesso ao banco de dados

--- conf/conf.php
+++ conf/conf.php
@@ -1,5 +1,31 @@
 <?php
 
-
 return array(
+    'basePath' => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..',
+    'name' => 'Curso',
+    'instituicao' => 'Framework Maestro - Curso',
+    'import' => array(
+        'models.*'
+    ),
+    'theme' => array(
+        'name' => 'default',
+        'template' => 'index'
+    ),
+    'ui' => array(
+        'inlineFormAction' => true
+    ),
+    'login' => array(
+        'module' => "",
+        'class' => "MAuthDbMd5",
+        'check' => false
+    ),
+    'db' => array(
+        'curso' => array(
+            'driver' => 'sqlite3',
+            'path' => Manager::getAppPath('curso.db'),
+            'formatDate' => '%d/%m/%Y',
+            'formatTime' => '%H:%M:%S',
+            'configurationClass' => 'Doctrine\DBAL\Configuration',
+        ),
+    ),
 );
